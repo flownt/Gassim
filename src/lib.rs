07a168f1930f0aa15fe3a465e489a383b009f8d3
F. E. C. Ruijter <flo.ruijt@hotmail.com>
2024-02-05 01:39:53 +0100

    Implemented 2d-elastic collissions for 'spheres' of equal mass. Visualizations included.

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,133 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
+use nalgebra::*;
+
+const D : usize =2;
+
+
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Particle {
+    x : SVector<f64, D>,
+    xd: SVector<f64, D>,
+    radius: f64
+}
+
+pub struct BoundBox {
+    lower: SVector<f64, D>,
+    upper: SVector<f64, D>
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, rs-wasm-gassim!");
+pub struct ParticleBox {
+        bounds: BoundBox,
+        particles: Vec<Particle>,
+}
+
+#[wasm_bindgen]
+impl ParticleBox {
+    pub fn tick(&mut self, dt: f64){
+        let mut next = self.particles.clone();
+
+        for i in 0..self.particles.len(){
+            let mut new_part = self.particles[i].clone();
+            new_part.x = self.particles[i].x + self.particles[i].xd * dt;
+
+            for j in 0..D{
+                // position of lower edge of the particle relative to the lower bound.
+                let pos_rel_lower = (new_part.x[j] - new_part.radius) - self.bounds.lower[j];
+
+                // position of upper edge of the particle relative to the upper bound.
+                let pos_rel_upper = (new_part.x[j] + new_part.radius) - self.bounds.upper[j];
+                if pos_rel_lower < 0.0 {
+
+                    new_part.x[j] = self.bounds.lower[j] + new_part.radius - pos_rel_lower;
+
+                    new_part.xd[j] = -self.particles[i].xd[j];
+                } else if pos_rel_upper > 0.0 {
+
+                    new_part.x[j] = self.bounds.upper[j] - new_part.radius - pos_rel_upper;
+
+                    new_part.xd[j] = -self.particles[i].xd[j];
+                }
+
+            }
+
+            next[i] = new_part;
+        }
+
+        for i in 0..self.particles.len(){
+            for j in (i+1)..self.particles.len(){
+                let delta = next[j].x - next[i].x;
+                if delta.norm() < (next[i].radius + next[j].radius){
+                    //Collision.
+                    if (next[j].xd - next[i].xd).dot(&delta) > 0.0{
+                        // j is already moving away. swapping will reinforce collision.
+                        continue
+                    }
+
+                    let vi_para = (next[i].xd.dot(&delta)/delta.dot(&delta)) * delta;
+                    let vi_perp = next[i].xd - vi_para;
+
+                    let vj_para = (next[j].xd.dot(&delta)/delta.dot(&delta)) * delta;
+                    let vj_perp = next[j].xd - vj_para;
+
+                    next[i].xd = vi_perp + vj_para;
+                    next[j].xd = vj_perp + vi_para;
+                }
+            }
+        }
+
+        self.particles = next;
+    }
+
+    pub fn text_render(&mut self) -> String{
+        return self.particles.iter().fold("".to_string(), |acc, item| format!("{acc}\nx: {:?}, \tv: {:?}", item.x, item.xd))
+    }
+
+    pub fn particles_ptr(&self) -> *const f64 {
+        return self.particles[0].x.as_ptr()
+    }
+    pub fn particles_len(&self) -> usize{
+        return self.particles.len()
+    }
+
+    pub fn particle_size(&self) -> usize{
+        return 2*D+1
+    }
+
+    pub fn bbox_ptr(&self) -> *const f64 {
+        return self.bounds.lower.as_ptr()
+    }
+
+
+
+    pub fn new() -> ParticleBox {
+        let lower = vector![0.0, 0.0];
+        let upper = vector![1.0, 1.0];
+        let delta = upper-lower;
+
+        let bbox = BoundBox{
+            lower,
+            upper,
+        };
+
+        const N: usize = 2500;
+        let mut particles: Vec<Particle> = Vec::with_capacity(N);
+
+        for _i in 0..N {
+            let v1 = lower + delta.component_mul(&SVector::<f64, D>::new_random());
+            let mut v2 = Vector2::new_random()-Vector2::from_element(0.5);
+
+            v2 = v2.normalize() * 0.25;
+
+            v2 = 0.1*delta.component_mul(&v2);
+
+            let p = Particle{x: v1, xd: v2, radius: 0.0030};
+            particles.push(p);
+        }
+
+        return ParticleBox {bounds: bbox , particles};
+    }
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,192 @@
-import * as wasm from "hello-wasm-pack";
+import { ParticleBox } from "rs-wasm-gassim";
 
-wasm.greet();
+import { memory } from "rs-wasm-gassim/rs_wasm_gassim_bg.wasm";
+
+const pre = document.getElementById("parts-pre");
+
+const canvas = document.getElementById("parts-canvas");
+const graph_cv = document.getElementById("graph-canvas");
+
+canvas.height = 600;
+canvas.width = 600;
+const ctx = canvas.getContext('2d');
+
+graph_cv.height = canvas.height
+graph_cv.width  = canvas.width
+
+const gf_ctx = graph_cv.getContext('2d');
+
+const particleBox = ParticleBox.new();
+console.log(particleBox)
+
+window.pb = particleBox
+
+let particles_ptr = particleBox.particles_ptr();
+let particles_len = particleBox.particles_len();
+let particle_size = particleBox.particle_size();
+let particles = new Float64Array(memory.buffer, particles_ptr, particles_len * particle_size);
+
+let scaling = 1.0;
+
+const D = 2;
+
+const loadParticles = () => {
+  particles_ptr = particleBox.particles_ptr();
+  particles_len = particleBox.particles_len();
+  particle_size = particleBox.particle_size();
+  particles = new Float64Array(memory.buffer, particles_ptr, particles_len * particle_size);
+
+  const bounds = new Float64Array(memory.buffer,
+                            particleBox.bbox_ptr(),
+                            2*D)
+
+  scaling = Math.min(canvas.width/(bounds[2]-bounds[0]),
+                     canvas.height/bounds[3]-bounds[1])
+}
+
+window.ps_f64 = particles
+
+
+
+const drawEdge = () => {
+  ctx.beginPath()
+  ctx.rect(1, 1, canvas.width-1, canvas.height-1)
+  ctx.stroke()
+}
+
+const drawParticles = () => {
+
+  for(let i =0; i < particles_len; i++){
+    const x = particles[i*particle_size+0] * scaling
+    const y = particles[i*particle_size+1] * scaling
+    const part_radius = particles[i*particle_size + 4] * scaling
+
+    // if(i == 0){
+    //   console.log('part[0].x = ', [x, y], 'r = ', part_radius)
+    // }
+
+    ctx.beginPath()
+    ctx.arc(x, y, part_radius, 0, 7)
+    // ctx.strokeText(i, x, y)
+    ctx.stroke()
+  }
+
+
+}
+
+const drawGraph = () => {
+  const min_v =  0.0;
+  const max_v =  0.1;
+
+  const n_buckets = 25;
+  const bucket_length = (max_v - min_v)/n_buckets;
+
+  let buckets_cnt = Array(n_buckets).fill(0);
+  let buckets_lower = Array(n_buckets)
+  let buckets_upper = Array(n_buckets)
+
+  for(var i=0;i<n_buckets;i++){
+    buckets_lower[i] = min_v +     i*bucket_length;
+    buckets_upper[i] = min_v + (i+1)*bucket_length;
+  }
+
+  let v = Array(particles_len)
+
+  for(let i =0; i < particles_len; i++){
+    const vx = particles[i*particle_size+2]
+    const vy = particles[i*particle_size+3]
+
+    v[i] = Math.sqrt(vx*vx + vy*vy);
+  }
+  v = v.sort()
+
+  let E =0
+
+  let j=0;
+  for(let i =0; i < particles_len; i++){
+    while(v[i] > buckets_upper[j] && j < n_buckets) j++
+
+    buckets_cnt[j] +=1;
+    E+=v[i]*v[i]
+  }
+
+  const v_rms = Math.sqrt(E/particles_len)
+
+  const transform_x = (v_coord) => {
+    return (v_coord - min_v)/(max_v - min_v) * graph_cv.width
+  }
+
+  const transform_y = (cnt) => {
+    return (cnt)/(particles_len/3) * (-graph_cv.height) +  .5* graph_cv.height
+  }
+
+
+  gf_ctx.beginPath()
+  gf_ctx.moveTo(0, graph_cv.height/2);
+  for(var i=0;i<n_buckets;i++){
+    buckets_lower[i] = min_v +     i*bucket_length;
+    buckets_upper[i] = min_v + (i+1)*bucket_length;
+    const x = transform_x((buckets_lower[i]+buckets_upper[i])/2);
+    const y = transform_y(buckets_cnt[i])
+    gf_ctx.lineTo(x, y);
+  }
+  gf_ctx.stroke()
+
+  {
+    gf_ctx.beginPath()
+    const x = transform_x(v_rms)
+    gf_ctx.moveTo(x, 0);
+    gf_ctx.lineTo(x, graph_cv.height);
+    gf_ctx.stroke()
+  }
+}
+
+let animationId = null;
+
+const playPauseButton = document.getElementById("play-pause");
+
+const play = () => {
+  playPauseButton.textContent = "⏸";
+  last_render = Date.now()
+  renderLoop();
+};
+
+const pause = () => {
+  playPauseButton.textContent = "▶";
+  cancelAnimationFrame(animationId);
+  animationId = null;
+};
+
+playPauseButton.addEventListener("click", event => {
+  if (animationId === null) {
+    play();
+  } else {
+    pause();
+  }
+});
+
+let last_render = Date.now()
+
+const renderLoop = () => {
+  let now = Date.now()
+  const dt = (now-last_render)/1000
+  last_render = now
+
+  particleBox.tick(dt);
+  loadParticles()
+  pre.textContent = 'fps: ' + Math.round(1/dt)
+//  pre.textContent = particleBox.text_render();
+
+
+  ctx.clearRect(1, 1, canvas.width-1, canvas.height-1);
+  gf_ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawEdge()
+  drawParticles();
+  drawGraph()
+
+
+  animationId = requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
